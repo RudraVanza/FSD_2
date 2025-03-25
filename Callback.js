9c7865bd74dee1054eed30ca312f751311f33446
RudraVanza <rudravanza0909@gmail.com>
2025-03-25 12:22:06 +0530

    Add files via upload

---
+++ Callback.js
@@ -0,0 +1,15 @@
+// const msg = function (params) {
+//     console.log("This message is show after 3 Second");
+// }
+// // setTimeout(msg,3000);  //This print 1 time after 3 second
+
+// var a = 2;
+// const msg = function(){
+//     console.log(a++);
+// }
+// setInterval(msg,1000);      // this print after every 3 second continues
+
+// setTimeout(function(){console.log("Hello");},1000);    // 2nd way 
+// setTimeout(()=>{console.log("hello")},1000)
+
+
